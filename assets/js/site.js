46e868895c07b11d211926a0d4d1920afc22552c
kimbach91-prog <kimbach91@gmail.com>
2026-08-30 14:23:51 +0700

    Expose Academic Democracy as a first-class site navigation item

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,14 @@
-(()=>{const links=[...document.querySelectorAll('a[href^="#"]')];for(const a of links)a.addEventListener('click',()=>history.replaceState(null,'',a.getAttribute('href')));})();
+(()=>{
+  const links=[...document.querySelectorAll('a[href^="#"]')];
+  for(const a of links)a.addEventListener('click',()=>history.replaceState(null,'',a.getAttribute('href')));
+
+  const nav=document.querySelector('.top nav');
+  if(!nav||nav.querySelector('a[href$="academic-democracy.html"]'))return;
+  const theory=nav.querySelector('a[href$="theory.html"]');
+  if(!theory)return;
+  const href=theory.getAttribute('href').replace(/theory\.html$/, 'academic-democracy.html');
+  const a=document.createElement('a');
+  a.href=href;
+  a.textContent='Dân chủ Học thuật';
+  theory.insertAdjacentElement('afterend',a);
+})();
