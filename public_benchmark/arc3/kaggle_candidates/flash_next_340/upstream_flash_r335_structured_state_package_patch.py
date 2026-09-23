e447186794040852c7738eb730bd69427e954934
kimbach91-prog <kimbach91@gmail.com>
2026-09-23 21:20:26 +0700

    arc3: repair static R335 selftest newline construction

--- public_benchmark/arc3/kaggle_candidates/flash_next_340/upstream_flash_r335_structured_state_package_patch.py
+++ public_benchmark/arc3/kaggle_candidates/flash_next_340/upstream_flash_r335_structured_state_package_patch.py
@@ -142,7 +142,7 @@ def patch_package(root: Path) -> dict:
     exec(PATCH_SOURCE, ns)
     anchor = ns["R335_WORLD_MODEL_ANCHOR"]
     extension = ns["R335_WORLD_MODEL_EXTENSION"]
-    sample = 'PROMPT = (\\n    "' + anchor + '",\\n)\\n'
+    sample = 'PROMPT = (\n    "' + anchor + '",\n)\n'
     patched_sample = sample.replace(anchor, extension, 1)
     compile(patched_sample, "r335_prompt_escape_selftest.py", "exec")
     if SCHEMA_MARKER not in patched_sample:
